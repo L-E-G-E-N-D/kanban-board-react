7e7cb5be3c2cec8aa17b418c2ea5fb23e1286830
Tanush <tanushkumar777@gmail.com>
2026-01-17 12:59:17 +0530

    Add nodemon for backend development

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
+
 const express=require('express');
 const cors=require('cors');
 const app=express();
 app.use(cors());
 app.use(express.json());
 
+
+let tasks = [
+  { id: 1, title: "Design UI", status: "todo" },
+  { id: 2, title: "Build backend", status: "doing" },
+  { id: 3, title: "Setup project", status: "done" }
+];
+
+
 app.get('/health', (req, res) => {
     res.send("Backend running");
 });
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);
+});
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
